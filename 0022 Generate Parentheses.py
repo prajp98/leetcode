877379be88de9471c00c152c6f39627087b169d4
Prajwala <praj.p98@gmail.com>
2025-03-01 22:43:54 +0530

    22

---
+++ 0022 Generate Parentheses.py
@@ -0,0 +1,17 @@
+def generateParenthesis(self, n: int) -> List[str]:
+    res = []
+
+    def dfs(s, countl, countr):
+        if len(s) == n * 2:
+            res.append("".join(s))
+        if countl < n:
+            s.append("(")
+            dfs(s, countl + 1, countr)
+            s.pop()
+        if countr < countl:
+            s.append(")")
+            dfs(s, countl, countr + 1)
+            s.pop()
+
+    dfs([], 0, 0)
+    return res
