b4588727e2cd23c139987243f9bb6e2bb3c832d3
Prajwala <praj.p98@gmail.com>
2025-03-01 22:46:09 +0530

    22

---
+++ 0155 Min Stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+        self.mins=[]
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        top=self.mins[-1] if self.mins else val
+        self.mins.append(min(val,top))
+
+    def pop(self) -> None:
+        self.mins.pop()
+        self.s.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
