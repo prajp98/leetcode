c13e07c58812be2d88cdef616e16e574b56d3fa0
Prajwala <praj.p98@gmail.com>
2025-03-04 01:17:31 +0530

    1475. Final Prices With a Special Discount in a Shop

---
+++ 1475 Final Prices With a Special Discount in a Shop.py
@@ -0,0 +1,8 @@
+def finalPrices(self, prices: List[int]) -> List[int]:
+    res = prices[:]
+    stack = []
+    for i, price in enumerate(prices):
+        while stack and prices[stack[-1]] >= price:
+            res[stack.pop()] -= price
+        stack.append(i)
+    return res
