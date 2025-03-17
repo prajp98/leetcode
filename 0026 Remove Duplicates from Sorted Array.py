e979595fa1182745070d972745b019c2e861ccae
prajwalaparameshwar <praj.p98@gmail.com>
2025-03-17 17:20:27 +0530

    26. Remove Duplicates from Sorted Array

---
+++ 0026 Remove Duplicates from Sorted Array.py
@@ -0,0 +1,9 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+    if not nums:
+        return 0
+    l = 0
+    for r in range(1, len(nums)):
+        if nums[r] != nums[l]:
+            l += 1
+            nums[l] = nums[r]
+    return l + 1
