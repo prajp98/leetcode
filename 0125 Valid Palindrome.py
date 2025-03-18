2f81e48f54f7cdd50427ec681228d2fe6776e30b
prajwalaparameshwar <praj.p98@gmail.com>
2025-03-18 19:32:32 +0530

    125. Valid Palindrome

---
+++ 0125 Valid Palindrome.py
@@ -0,0 +1,19 @@
+def isPalindrome(self, s: str) -> bool:
+    t = []
+    for ch in s:
+        if ch.isalnum():
+            t.append(ch.lower())
+    return t == t[::-1]
+
+def isPalindrome(self, s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l < r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+    return True
