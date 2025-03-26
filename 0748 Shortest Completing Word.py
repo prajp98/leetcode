4549dea52f0e203ae835037fac911c8e49c0378b
Prajwala <praj.p98@gmail.com>
2025-03-26 20:06:10 +0530

    748. Shortest Completing Word

--- 0748 Shortest Completing Word.py
+++ 0748 Shortest Completing Word.py
@@ -6,13 +6,14 @@ def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
         if ch.isalpha():
             count[ch.lower()] += 1
     for word in words:
-        if countWord=Counter(word)
-        flag = True
-        for ch, c in count.items():
-            if c > countWord[ch]:
-                flag = False
-                break
-        if len(word) < minl and flag:
-            res = word
-            minl = len(word)
+        if len(word) < minl:
+            countWord = Counter(word)
+            flag = True
+            for ch, c in count.items():
+                if c > countWord[ch]:
+                    flag = False
+                    break
+            if len(word) < minl and flag:
+                res = word
+                minl = len(word)
     return res
