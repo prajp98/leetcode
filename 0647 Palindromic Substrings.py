7ceb916d991cedc7fae3554d7b0cdb41a497b29c
Prajwala <praj.p98@gmail.com>
2025-03-27 15:57:12 +0530

    647. Palindromic Substrings

--- 0647 Palindromic Substrings.py
+++ 0647 Palindromic Substrings.py
@@ -34,6 +34,23 @@ def countSubstrings(self, s: str) -> int:
                 count+=1
     return count
 
+def countSubstrings(self, s: str) -> int:
+    n = len(s)
+    dp = [[False] * n for _ in range(n)]
+    count = 0
+
+    for i in range(n):
+        dp[i][i] = True
+        count += 1
+
+    for length in range(2, n + 1):  # Length of substring
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if s[i] == s[j] and (length == 2 or dp[i+1][j-1]):
+                dp[i][j] = True
+                count += 1
+
+    return count
 
 #Expand around center O(n**2) O(1)
 def countSubstrings(self, s: str) -> int:
