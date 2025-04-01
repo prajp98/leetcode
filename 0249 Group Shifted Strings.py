014af05e4853980d6e598b21df759c1e9aeb6044
Prajwala <praj.p98@gmail.com>
2025-04-01 13:20:43 +0530

    249. Group Shifted Strings

---
+++ 0249 Group Shifted Strings.py
@@ -0,0 +1,6 @@
+def groupStrings(self, strings: List[str]) -> List[List[str]]:
+    mp = defaultdict(list)
+    for s in strings:
+        key = tuple((ord(c) - ord(s[0])) % 26 for c in s)
+        mp[key].append(s)
+    return list(mp.values())
