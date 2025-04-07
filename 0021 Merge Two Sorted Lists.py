4ef8f3757e614480292a6124f1b1c503da79b382
Prajwala <praj.p98@gmail.com>
2025-04-07 15:10:11 +0530

    21. Merge Two Sorted Lists

--- 0021 Merge Two Sorted Lists.py
+++ 0021 Merge Two Sorted Lists.py
@@ -14,3 +14,20 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
     if p2:
         curr.next = p2
     return dummy.next
+
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    p1,p2=list1,list2
+    dummy=curr=ListNode()
+    while p1 and p2:
+        if p1.val<=p2.val:
+            curr.next=p1
+            p1=p1.next
+        else:
+            curr.next=p2
+            p2=p2.next
+        curr=curr.next
+    if p1:
+        curr.next=p1
+    if p2:
+        curr.next=p2
+    return dummy.next
