6502e5a0d2be286452f63a9653458a8d77d9bb5c
Prajwala <praj.p98@gmail.com>
2025-04-10 00:11:01 +0530

    23. Merge k Sorted Lists

--- 0023 Merge k Sorted Lists.py
+++ 0023 Merge k Sorted Lists.py
@@ -23,3 +23,17 @@ def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
             newList.append(merge(l1, l2))
         lists = newList
     return lists[0]
+
+def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    heap = []
+    for i,node in enumerate(lists):
+        if node:
+            heapq.heappush(heap,(node.val,i,node))
+    dummy = curr = ListNode()
+    while heap:
+        val,i,node=heapq.heappop(heap)
+        curr.next=node
+        curr=curr.next
+        if node.next:
+            heapq.heappush(heap,(node.next.val,i,node.next))
+    return dummy.next
