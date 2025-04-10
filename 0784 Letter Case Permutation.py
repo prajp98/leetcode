578c7e354143320811720ecb15b35cc482785ac2
Prajwala <praj.p98@gmail.com>
2025-04-10 23:09:22 +0530

    784. Letter Case Permutation

--- 0784 Letter Case Permutation.py
+++ 0784 Letter Case Permutation.py
@@ -14,3 +14,23 @@ def letterCasePermutation(self, s: str) -> List[str]:
 
     dfs(0, "")
     return res
+
+def letterCasePermutation(self, s: str) -> List[str]:
+    res=[]
+    def dfs(i,path):
+        if i==len(s):
+            res.append(''.join(path))
+            return
+        if s[i].isalpha():
+            path.append(s[i].lower())
+            dfs(i+1,path)
+            path.pop()
+            path.append(s[i].upper())
+            dfs(i+1,path)
+            path.pop()
+        else:
+            path.append(s[i])
+            dfs(i+1,path)
+            path.pop()
+    dfs(0,[])
+    return res
