239dfc37e21aa289b2d166f90a4974614d44d6f9
Prajwala <praj.p98@gmail.com>
2025-04-11 17:03:32 +0530

    216. Combination Sum III

--- 0046 Permutations.py
+++ 0046 Permutations.py
@@ -1,13 +1,15 @@
 def permute(self, nums: List[int]) -> List[List[int]]:
     res = []
-    def dfs(nums, cur):
-        if not nums:
-            res.append(cur[::])
+
+    def dfs(path):
+        if len(path) == len(nums):
+            res.append(path[:])
             return
-        for i in range(len(nums)):
-            newNums = nums[:i] + nums[i + 1:]
-            cur.append(nums[i])
-            dfs(newNums, cur)
-            cur.pop()
-    dfs(nums, [])
+        for num in nums:
+            if num not in path:
+                path.append(num)
+                dfs(path)
+                path.pop()
+
+    dfs([])
     return res
