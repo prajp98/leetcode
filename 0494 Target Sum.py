714bffd458db2d668a6135bdd2981647039a13f6
Prajwala <praj.p98@gmail.com>
2025-04-11 23:49:17 +0530

    494. Target Sum

--- 0494 Target Sum.py
+++ 0494 Target Sum.py
@@ -12,3 +12,14 @@ def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
     dfs(0, 0)
     return count
+
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    memo = {}
+    def dfs(i, total):
+        if i == len(nums):
+            return 1 if total == target else 0
+        if (i, total) in memo:
+            return memo[(i, total)]
+        memo[(i,total)]= dfs(i+1,total+nums[i])+dfs(i+1,total-nums[i])
+        return memo[(i, total)]
+    return dfs(0, 0)
