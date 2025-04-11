02d2d4bfc1d9eac195084092b6ad246c141fb3e6
Prajwala <praj.p98@gmail.com>
2025-04-11 20:32:39 +0530

    357. Count Numbers with Unique Digits

--- 0357 Count Numbers with Unique Digits.py
+++ 0357 Count Numbers with Unique Digits.py
@@ -1,4 +1,22 @@
 def countNumbersWithUniqueDigits(self, n: int) -> int:
+    count = 1
+    def dfs(i, seen):
+        nonlocal count
+        if i == n:
+            return
+        for digit in range(10):
+            if i == 0 and digit == 0:
+                continue
+            if digit not in seen:
+                seen.add(digit)
+                count += 1
+                dfs(i + 1, seen)
+                seen.remove(digit)
+
+    dfs(0, set())
+    return count
+
+def countNumbersWithUniqueDigits(self, n: int) -> int:
     if n == 0:
         return 1
     res = 10
