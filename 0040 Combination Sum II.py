a67ec29c506ae9890e76f91d9cb632ed05ee2bda
Prajwala <praj.p98@gmail.com>
2025-04-12 15:50:58 +0530

    40. Combination Sum II

--- 0040 Combination Sum II.py
+++ 0040 Combination Sum II.py
@@ -17,3 +17,22 @@ def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]
     candidates.sort()
     dfs(0, [], 0)
     return res
+
+def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    res = []
+    candidates.sort()
+    def backtrack(start, path, total):
+        if total == target:
+            res.append(path[:])
+            return
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+            if total + candidates[i] > target:
+                break
+            path.append(candidates[i])
+            backtrack(i + 1, path, total + candidates[i])
+            path.pop()
+
+    backtrack(0, [], 0)
+    return res
