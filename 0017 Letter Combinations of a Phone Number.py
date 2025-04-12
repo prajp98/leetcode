1e1713e7eaad9328cb1fc2d6d07a8aeb460c5a44
Prajwala <praj.p98@gmail.com>
2025-04-12 14:32:47 +0530

    17. Letter Combinations of a Phone Number

--- 0017 Letter Combinations of a Phone Number.py
+++ 0017 Letter Combinations of a Phone Number.py
@@ -1,24 +1,16 @@
 def letterCombinations(self, digits: str) -> List[str]:
     res = []
-    hashMap = {
-        "2": "abc",
-        "3": "def",
-        "4": "ghi",
-        "5": "jkl",
-        "6": "mno",
-        "7": "qprs",
-        "8": "tuv",
-        "9": "wxyz",
-    }
-    curr = ""
+    if not digits:
+        return []
+    digitToChar = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
-    def dfs(i, curr):
-        if len(curr) == len(digits):
-            res.append(curr)
+    def dfs(i, s):
+        if i == len(digits):
+            res.append(s)
             return
-        for c in hashMap[digits[i]]:
-            dfs(i + 1, curr + c)
+        letters = digitToChar[digits[i]]
+        for c in letters:
+            dfs(i + 1, s + c)
 
     dfs(0, "")
-    if digits:
-        return res
+    return res
