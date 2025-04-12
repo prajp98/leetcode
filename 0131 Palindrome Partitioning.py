8be499587fd08a1ad7d503b4524395ce2ba721cd
Prajwala <praj.p98@gmail.com>
2025-04-12 16:10:59 +0530

    131. Palindrome Partitioning

--- 0131 Palindrome Partitioning.py
+++ 0131 Palindrome Partitioning.py
@@ -1,17 +1,19 @@
 def partition(self, s: str) -> List[List[str]]:
     res = []
-    part = []
-    def isPali(sub):
-        return sub==sub[::-1]
-    def dfs(i):
-        if i>=len(s):
-            res.append(part[::])
+
+    def pali(s):
+        return s == s[::-1]
+
+    def dfs(start, path):
+        if start == len(s):
+            res.append(path[:])
             return
-        for j in range(i,len(s)):
-            sub=s[i:j+1]
-            if isPali(sub):
-                part.append(sub)
-                dfs(j+1)
-                part.pop()
-    dfs(0)
+        for end in range(start + 1, len(s) + 1):
+            sub = s[start:end]
+            if pali(sub):
+                path.append(sub)
+                dfs(end, path)
+                path.pop()
+
+    dfs(0, [])
     return res
