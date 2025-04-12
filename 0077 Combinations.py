07d07de523b78b1ae52966ee5446815402fc190a
Prajwala <praj.p98@gmail.com>
2025-04-12 13:04:54 +0530

    681. Next Closest Time

--- 0077 Combinations.py
+++ 0077 Combinations.py
@@ -12,3 +12,17 @@ def combine(self, n: int, k: int) -> List[List[int]]:
 
     backtrack([], 1)
     return res
+
+def combine(self, n: int, k: int) -> List[List[int]]:
+    res = []
+    def dfs(start, path):
+        if len(path) == k:
+            res.append(path[:])
+            return
+        # We only loop till n - (k - len(path)) + 1
+        for i in range(start, n - (k - len(path)) + 2):
+            path.append(i)
+            dfs(i + 1, path)
+            path.pop()
+    dfs(1, [])
+    return res
