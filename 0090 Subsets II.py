a4abb9e4e9c577d1307f804c980ba1c22219fdbf
Prajwala <praj.p98@gmail.com>
2025-04-12 14:29:05 +0530

    90. Subsets II

--- 0090 Subsets II.py
+++ 0090 Subsets II.py
@@ -16,3 +16,20 @@ def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
 
     dfs(0)
     return res
+
+def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    res = []
+    nums.sort()  # Sort to handle duplicates easily
+
+    def dfs(start, path):
+        res.append(path[:])
+        for i in range(start, len(nums)):
+            # Skip duplicates
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            path.append(nums[i])
+            dfs(i + 1, path)
+            path.pop()
+
+    dfs(0, [])
+    return res
