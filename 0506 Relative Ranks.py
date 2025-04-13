a16a3e675a8ef30d7d8155f6ff6800decc9d43e9
Prajwala <praj.p98@gmail.com>
2025-04-13 19:14:28 +0530

    506. Relative Ranks

--- 0506 Relative Ranks.py
+++ 0506 Relative Ranks.py
@@ -1,4 +1,22 @@
-
+def findRelativeRanks(self, score: List[int]) -> List[str]:
+    heap = []
+    n = len(score)
+    res = [0] * n
+    for i, num in enumerate(score):
+        heapq.heappush(heap, (-num, i))
+    j = 1
+    while heap:
+        num, i = heapq.heappop(heap)
+        if j == 1:
+            res[i] = "Gold Medal"
+        elif j == 2:
+            res[i] = "Silver Medal"
+        elif j == 3:
+            res[i] = "Bronze Medal"
+        else:
+            res[i] = str(j)
+        j += 1
+    return res
 def findRelativeRanks(self, score: List[int]) -> List[str]:
     maxScore = max(score)
     scoreToIndex = [-1] * (maxScore + 1)
