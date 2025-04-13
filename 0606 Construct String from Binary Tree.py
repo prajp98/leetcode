ae172bae1a0d55c4ad23ec079b755bf29d2b6a26
Prajwala <praj.p98@gmail.com>
2025-04-14 00:45:25 +0530

    606. Construct String from Binary Tree

---
+++ 0606 Construct String from Binary Tree.py
@@ -0,0 +1,11 @@
+def tree2str(self, root: Optional[TreeNode]) -> str:
+    def dfs(root):
+        if not root:
+            return ""
+        if root.right:
+            return str(root.val) + "(" + dfs(root.left) + ")" + "(" + dfs(root.right) + ")"
+        if root.left:
+            return str(root.val) + "(" + dfs(root.left) + ")"
+        return str(root.val)
+
+    return dfs(root)
