dcab09813a91bde5fa97fac54ccb11d3c08dad9c
Prajwala <praj.p98@gmail.com>
2025-04-13 23:41:39 +0530

    104. Maximum Depth of Binary Tree

--- 0104 Maximum Depth of Binary Tree.py
+++ 0104 Maximum Depth of Binary Tree.py
@@ -17,3 +17,12 @@ def maxDepth(self, root: Optional[TreeNode]) -> int:
                 q.append(node.right)
         l += 1
     return l
+
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return 0
+            left=dfs(root.left)
+            right=dfs(root.right)
+            return 1+max(left,right)
+        return dfs(root)
