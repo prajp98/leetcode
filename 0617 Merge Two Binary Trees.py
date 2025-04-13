cb9b465b26158d6edafbf9de94772da9c67fa1e7
Prajwala <praj.p98@gmail.com>
2025-04-13 22:28:01 +0530

    617. Merge Two Binary Trees

--- 0617 Merge Two Binary Trees.py
+++ 0617 Merge Two Binary Trees.py
@@ -9,3 +9,17 @@ def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Op
             return p or q
 
     return dfs(root1, root2)
+
+def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    def dfs(p,q):
+        if not p and not q:
+            return None
+        if not p:
+            return q
+        if not q:
+            return p
+        p.val+=q.val
+        p.left=dfs(p.left,q.left)
+        p.right=dfs(p.right,q.right)
+        return p
+    return dfs(root1,root2)
