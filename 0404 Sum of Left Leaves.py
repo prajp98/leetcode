88108912327c08a4ad4ff5da2f2d0a18707a9d79
Prajwala <praj.p98@gmail.com>
2025-04-14 11:45:27 +0530

    404. Sum of Left Leaves

--- 0404 Sum of Left Leaves.py
+++ 0404 Sum of Left Leaves.py
@@ -5,5 +5,4 @@ def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         if not root.left and not root.right:
             return root.val if l else 0
         return dfs(root.left, True) + dfs(root.right, False)
-
     return dfs(root, False)
