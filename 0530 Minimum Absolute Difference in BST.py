2246e007c03ed86da840e59b780d6c0d20be9472
Prajwala <praj.p98@gmail.com>
2025-04-14 12:59:35 +0530

    530. Minimum Absolute Difference in BST

--- 0530 Minimum Absolute Difference in BST.py
+++ 0530 Minimum Absolute Difference in BST.py
@@ -13,3 +13,18 @@ def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
     for i in range(len(l) - 1):
         res = min(res, l[i + 1] - l[i])
     return res
+
+def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+    res=float('inf')
+    prev=None
+    def dfs(root):
+        nonlocal res,prev
+        if not root:
+            return
+        dfs(root.left)
+        if prev is not None:
+            res=min(res,root.val-prev)
+        prev=root.val
+        dfs(root.right)
+    dfs(root)
+    return res
