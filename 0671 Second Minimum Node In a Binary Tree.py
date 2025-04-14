fb0e34c7dc642f4eaf06942d239d93f1ca145571
Prajwala <praj.p98@gmail.com>
2025-04-14 16:19:41 +0530

    671. Second Minimum Node In a Binary Tree

--- 0671 Second Minimum Node In a Binary Tree.py
+++ 0671 Second Minimum Node In a Binary Tree.py
@@ -1,16 +1,16 @@
 def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
-    min1 = min2 = float('inf')
+    min1 = root.val
+    min2 = float('inf')
 
     def dfs(root):
         nonlocal min1, min2
         if not root:
             return
-        if root.val < min1:
-            min2, min1 = min1, root.val
-        elif min2 > root.val > min1:
+        if min2 > root.val > min1:
             min2 = root.val
-        dfs(root.left)
-        dfs(root.right)
+        elif root.val == min1:
+            dfs(root.left)
+            dfs(root.right)
 
     dfs(root)
     return min2 if min2 < float('inf') else -1
