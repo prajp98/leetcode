63e6997a5f702f39f3be500b7ece3b196d68ec3f
Prajwala <praj.p98@gmail.com>
2025-04-16 21:31:39 +0530

    501. Find Mode in Binary Search Tree

--- 0501 Find Mode in Binary Search Tree.py
+++ 0501 Find Mode in Binary Search Tree.py
@@ -14,3 +14,24 @@ def findMode(self, root: Optional[TreeNode]) -> List[int]:
         if count[num] == m:
             res.append(num)
     return res
+
+def findMode(self, root: Optional[TreeNode]) -> List[int]:
+    currNum,maxStreak,currStreak,res=0,0,0,[]
+    def dfs(root):
+        nonlocal currNum,maxStreak,currStreak,res
+        if not root:
+            return
+        dfs(root.left)
+        if root.val==currNum:
+            currStreak+=1
+        else:
+            currNum=root.val
+            currStreak=1
+        if currStreak>maxStreak:
+            maxStreak=currStreak
+            res=[currNum]
+        elif currStreak==maxStreak:
+            res.append(currNum)
+        dfs(root.right)
+    dfs(root)
+    return res
