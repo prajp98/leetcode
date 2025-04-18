da50aa66a608917e57c193b97036a182fbe9c782
prajwalap <praj.p98@gmail.com>
2025-04-18 12:23:37 +0530

    623. Add One Row to Tree-2

--- 0623 Add One Row to Tree.py
+++ 0623 Add One Row to Tree.py
@@ -14,3 +14,28 @@ def addOneRow(self, root: TreeNode, val: int, depth: int) -> TreeNode:
 
     dfs(root, 1)
     return root
+
+def addOneRow(self, root: TreeNode, val: int, depth: int) -> TreeNode:
+    if depth == 1:
+        return TreeNode(val, left=root)
+
+    queue = deque([root])
+    current_depth = 1
+
+    while queue:
+        level_size = len(queue)
+        if current_depth == depth - 1:
+            for _ in range(level_size):
+                node = queue.popleft()
+                node.left = TreeNode(val, left=node.left)
+                node.right = TreeNode(val, right=node.right)
+            break
+        for _ in range(level_size):
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        current_depth += 1
+
+    return root
