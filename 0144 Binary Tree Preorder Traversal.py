fe462e88c8d3aabd9f7d68e033fe08a5c9a623d9
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-21 18:52:15 +0530

    144. Binary Tree Preorder Traversal

--- 0144 Binary Tree Preorder Traversal.py
+++ 0144 Binary Tree Preorder Traversal.py
@@ -1,4 +1,16 @@
 def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    res = []
+
+    def preorder(root):
+        if not root:
+            return
+        res.append(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+    preorder(root)
+    return res
+def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
     if not root:
         return []
     stack, res = [root], []
