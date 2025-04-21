b9a8c051260e5111f9d37acf21b19844ba111a4b
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-21 16:54:40 +0530

    652. Find Duplicate Subtrees-1

--- 0652 Find Duplicate Subtrees.py
+++ 0652 Find Duplicate Subtrees.py
@@ -13,3 +13,21 @@ def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeN
 
     dfs(root)
     return res
+
+def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+    subTreeMap = dict()
+    count = defaultdict(int)
+    res = []
+    def dfs(node):
+        if not node:
+            return 0
+        subTree = (dfs(node.left),node.val,dfs(node.right))
+        if subTree not in subTreeMap:
+            subTreeMap[subTree] = len(subTreeMap) + 1
+        uid = subTreeMap[subTree]
+        count[uid] += 1
+        if count[uid] == 2:
+            res.append(node)
+        return uid
+    dfs(root)
+    return res
