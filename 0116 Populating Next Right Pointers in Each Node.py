3f3373357dba1ad533a3ab073f61a2f2e13feb9f
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-21 15:01:26 +0530

    116. Populating Next Right Pointers in Each Node-2

--- 0116 Populating Next Right Pointers in Each Node.py
+++ 0116 Populating Next Right Pointers in Each Node.py
@@ -14,3 +14,21 @@ def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
             if node.right:
                 q.append(node.right)
     return root
+
+def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+    def dfs(node):
+        if not node or not node.left:
+            return
+
+        # Connect left -> right
+        node.left.next = node.right
+
+        # Connect right -> next.left (if next exists)
+        if node.next:
+            node.right.next = node.next.left
+
+        dfs(node.left)
+        dfs(node.right)
+
+    dfs(root)
+    return root
