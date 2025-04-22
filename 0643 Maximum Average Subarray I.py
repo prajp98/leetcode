876d5fc3d3678b0bddc9632956a82c3a3e4454f2
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-22 11:42:23 +0530

    643. Maximum Average Subarray I-3

--- 0643 Maximum Average Subarray I.py
+++ 0643 Maximum Average Subarray I.py
@@ -16,3 +16,13 @@ def findMaxAverage(self, nums: List[int], k: int) -> float:
         max_avg = max(max_avg, total / k)
     return max_avg
 
+
+def findMaxAverage(self, nums: List[int], k: int) -> float:
+    curr_sum = sum(nums[:k])
+    max_sum = curr_sum
+
+    for i in range(k, len(nums)):
+        curr_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum, curr_sum)
+
+    return max_sum / k
