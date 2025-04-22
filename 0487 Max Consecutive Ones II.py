4dcfebd329a770dae4265a356b3dcde7d9716175
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-22 18:11:37 +0530

    487. Max Consecutive Ones II

--- 0487 Max Consecutive Ones II.py
+++ 0487 Max Consecutive Ones II.py
@@ -11,3 +11,15 @@ def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
             left += 1
         maxi = max(maxi, right - left + 1)
     return maxi
+
+def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+    maxi = 0
+    last_zero = -1
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            left = last_zero + 1
+            last_zero = right
+        maxi = max(maxi, right - left + 1)
+
+    return maxi
