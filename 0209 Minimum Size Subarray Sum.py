e6cd6448063ad3ca2f35b2fa94f712324db1d3ca
prajwalaparameshwar <praj.p98@gmail.com>
2025-04-22 19:30:12 +0530

    209. Minimum Size Subarray Sum

---
+++ 0209 Minimum Size Subarray Sum.py
@@ -0,0 +1,13 @@
+def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    l = 0
+    s = 0
+    r = 0
+    res = float('inf')
+
+    for r in range(len(nums)):
+        s += nums[r]
+        while s >= target:
+            res = min(res, r - l + 1)
+            s -= nums[l]
+            l += 1
+    return res if res != float('inf') else 0
