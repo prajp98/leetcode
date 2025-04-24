d0da26b6c8f00e0cd8376aa053a547a94e805e93
Prajwala <praj.p98@gmail.com>
2025-04-24 14:33:27 +0530

    738. Monotone Increasing Digits

---
+++ 0738 Monotone Increasing Digits.py
@@ -0,0 +1,13 @@
+def monotoneIncreasingDigits(self, n: int) -> int:
+    digits = list(str(n))
+    mark = len(digits)
+
+    for i in range(len(digits) - 1, 0, -1):
+        if digits[i - 1] > digits[i]:
+            digits[i - 1] = str(int(digits[i - 1]) - 1)
+            mark = i  # Everything from here becomes 9
+
+    for i in range(mark, len(digits)):
+        digits[i] = '9'
+
+    return int("".join(digits))
