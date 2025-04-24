b02a6d6bc08f5085a37ec9223e23703f73baf343
Prajwala <praj.p98@gmail.com>
2025-04-24 16:34:38 +0530

    252. Meeting Rooms

---
+++ 0252 Meeting Rooms.py
@@ -0,0 +1,10 @@
+def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+    intervals.sort(key=lambda x: x[1])  # ✅ Sort by start time
+    if not intervals:
+        return True
+    prevEnd = intervals[0][1]
+    for i in range(1, len(intervals)):
+        if intervals[i][0] < prevEnd:
+            return False
+        prevEnd = intervals[i][1]
+    return True
