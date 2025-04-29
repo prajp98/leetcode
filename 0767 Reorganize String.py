2b5388e1f10e2544af9511f40782a94ce452e3db
Prajwala <praj.p98@gmail.com>
2025-04-29 17:29:59 +0530

    767. Reorganize String

--- 0767 Reorganize String.py
+++ 0767 Reorganize String.py
@@ -17,3 +17,25 @@ def reorganizeString(self, s: str) -> str:
         prevFreq = freq + 1
         prevChar = char
     return ''.join(res)
+
+def reorganizeString(self, s: str) -> str:
+    n = len(s)
+    count = Counter(s)
+    maxCh, maxFreq = max(count.items(), key=lambda x: x[1])
+    if maxFreq>(n+1)//2:
+        return ""
+    res = [""] * n
+    i = 0
+    while count[maxCh] > 0:
+        res[i] = maxCh
+        i+=2
+        count[maxCh] -= 1
+
+    for ch,freq in count.items():
+        while freq>0:
+            if i>=n:
+                i=1
+            res[i]=ch
+            i+=2
+            freq-=1
+    return ''.join(res)
