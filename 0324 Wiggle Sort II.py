f34bcea2879345b4038516323ef5739022e2ea5d
Prajwala <praj.p98@gmail.com>
2025-04-29 12:40:47 +0530

    324. Wiggle Sort II

--- 0324 Wiggle Sort II.py
+++ 0324 Wiggle Sort II.py
@@ -13,3 +13,9 @@ def wiggleSort(self, nums: List[int]) -> None:
         else:
             nums[i] = sorted_nums[end]
             end -= 1
+
+def wiggleSort(self, nums):
+    nums.sort()
+    mid = (len(nums) + 1) // 2
+    left, right = nums[:mid][::-1], nums[mid:][::-1]
+    nums[::2], nums[1::2] = left, right
