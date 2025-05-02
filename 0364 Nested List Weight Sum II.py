0df8ccc18041d57a68af2299ca4e879a2522ac9b
prajwalap <praj.p98@gmail.com>
2025-05-02 13:44:33 +0530

    364. Nested List Weight Sum II-bfs

--- 0364 Nested List Weight Sum II.py
+++ 0364 Nested List Weight Sum II.py
@@ -18,3 +18,18 @@ def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
         return total
 
     return dfs(nestedList, 1)
+
+def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+    q=deque(nestedList)
+    res=0
+    levelSum=0
+    while q:
+        for _ in range(len(q)):
+            e = q.popleft()
+            if e.isInteger():
+                levelSum += e.getInteger()
+            else:
+                for sub in e.getList():
+                    queue.append(sub)
+        res+=levelSum
+    return res
