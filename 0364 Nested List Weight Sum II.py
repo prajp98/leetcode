64137fc84fcf300e078ea32e8dd0fd68a104d336
prajwalap <praj.p98@gmail.com>
2025-05-02 12:42:55 +0530

    364. Nested List Weight Sum II-dfs

---
+++ 0364 Nested List Weight Sum II.py
@@ -0,0 +1,20 @@
+def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+    def getMaxDepth(l, d):
+        maxd = d
+        for item in l:
+            if not item.isInteger():
+                maxd = max(maxd, getMaxDepth(item.getList(), d + 1))
+        return maxd
+
+    maxDepth = getMaxDepth(nestedList, 1)
+
+    def dfs(l, d):
+        total = 0
+        for item in l:
+            if item.isInteger():
+                total += item.getInteger() * (maxDepth - d + 1)
+            else:
+                total += dfs(item.getList(), d + 1)
+        return total
+
+    return dfs(nestedList, 1)
