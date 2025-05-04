d881b73a0ba70c8ec2830277e5b65ff2c49167f8
Prajwala <praj.p98@gmail.com>
2025-05-04 21:49:17 +0530

    419

--- 0419 Battleships in a Board.py
+++ 0419 Battleships in a Board.py
@@ -1,10 +1,13 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        rows,cols=len(board),len(board[0])
+        rows, cols = len(board), len(board[0])
         count=0
         for r in range(rows):
             for c in range(cols):
-                if board[r][c]=="X":
-                    if (r==0 or board[r-1][c]==".") and (c==0 or board[r][c-1]=="."):
-                        count+=1
+                if board[r][c] == 'X':
+                    if r > 0 and board[r-1][c] == 'X':
+                        continue
+                    if c > 0 and board[r][c-1] == 'X':
+                        continue
+                    count += 1
         return count
