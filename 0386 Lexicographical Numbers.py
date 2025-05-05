6d48e09a6667ecd532e1a5768e6c228988a27d9a
Prajwala <praj.p98@gmail.com>
2025-05-05 16:11:25 +0530

    386. Lexicographical Numbers

--- 0386 Lexicographical Numbers.py
+++ 0386 Lexicographical Numbers.py
@@ -12,3 +12,16 @@ def lexicalOrder(self, n: int) -> List[int]:
     for i in range(1, 10):
         dfs(i, n)
     return res
+
+def lexicalOrder(self, n: int) -> List[int]:
+    res = []
+    curr = 1
+    for _ in range(n):
+        res.append(curr)
+        if curr * 10 <= n:
+            curr *= 10
+        else:
+            while curr % 10 == 9 or curr + 1 > n:
+                curr //= 10
+            curr += 1
+    return res
