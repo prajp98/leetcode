44c38ef5b7e6533f43f07ab7fd8493e199282763
Prajwala <praj.p98@gmail.com>
2025-05-06 13:00:04 +0530

    743. Network Delay Time-3

--- 0743 Network Delay Time.py
+++ 0743 Network Delay Time.py
@@ -29,3 +29,13 @@ def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
     dfs(k, 0)
     res = max(dist.values())
     return res if len(dist)==n else -1
+
+def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    dist = [float('inf')] * n
+    dist[k - 1] = 0
+    for _ in range(n - 1):
+        for u, v, w in times:
+            if dist[u - 1] + w < dist[v - 1]:
+                dist[v - 1] = dist[u - 1] + w
+    res = max(dist)
+    return res if res < float('inf') else -1
