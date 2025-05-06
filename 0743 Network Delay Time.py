b0a7598e04eb5edc0c79aea7cb9dd0c256737a21
Prajwala <praj.p98@gmail.com>
2025-05-06 12:59:08 +0530

    743. Network Delay Time-2

--- 0743 Network Delay Time.py
+++ 0743 Network Delay Time.py
@@ -13,3 +13,19 @@ def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
             if nei not in dist:
                 heapq.heappush(heap, (time + w, nei))
     return max(dist.values()) if len(dist) == n else -1
+
+def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    adj = defaultdict(list)
+    for u, v, w in times:
+        adj[u].append((v, w))
+    dist = {}
+
+    def dfs(node, time):
+        if node in dist and time >= dist[node]:
+            return
+        dist[node] = time
+        for nei, w in adj[node]:
+            dfs(nei, time + w)
+    dfs(k, 0)
+    res = max(dist.values())
+    return res if len(dist)==n else -1
