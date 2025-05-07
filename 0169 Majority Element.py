4da5d04876b37ba0ad69ee85e7971e503dc3e4f5
prajwalap <praj.p98@gmail.com>
2025-05-07 14:31:45 +0530

    169. Majority Element

---
+++ 0169 Majority Element.py
@@ -0,0 +1,8 @@
+def majorityElement(self, nums: List[int]) -> int:
+    count = 0
+    candidate = None
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += 1 if num == candidate else -1
+    return candidate
