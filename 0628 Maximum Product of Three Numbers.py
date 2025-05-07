b5dac42f4d4e7bc76d2abdc2ce88d83c0c29817f
prajwalap <praj.p98@gmail.com>
2025-05-07 17:20:08 +0530

    628. Maximum Product of Three Numbers-1

--- 0628 Maximum Product of Three Numbers.py
+++ 0628 Maximum Product of Three Numbers.py
@@ -2,3 +2,21 @@ def maximumProduct(self, nums: List[int]) -> int:
     nums.sort()
     return max(nums[-1] * nums[-2] * nums[-3],
                nums[0] * nums[1] * nums[-1])
+
+def maximumProduct(self, nums: List[int]) -> int:
+    max1 = max2 = max3 = -math.inf
+    min1 = min2 = math.inf
+    for n in nums:
+        if n > max1:
+            max1, max2, max3 = n, max1, max2
+        elif n > max2:
+            max2, max3 = n, max2
+        elif n > max3:
+            max3 = n
+
+        if n < min1:
+            min1, min2 = n, min1
+        elif n < min2:
+            min2 = n
+
+    return max(max1 * max2 * max3, max1 * min1 * min2)
