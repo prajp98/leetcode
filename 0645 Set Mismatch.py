783415c879d8ec87e6bd65567c58eda2f570b175
prajwalap <praj.p98@gmail.com>
2025-05-08 19:22:52 +0530

    645. Set Mismatch-2

--- 0645 Set Mismatch.py
+++ 0645 Set Mismatch.py
@@ -9,3 +9,15 @@ def findErrorNums(self, nums: List[int]) -> List[int]:
         if count[i] == 0:
             res[1] = i
     return res
+
+def findErrorNums(self, nums: List[int]) -> List[int]:
+    res=[]
+    for num in nums:
+        if (nums[abs(num)-1])<0:
+            res.append(abs(num))
+        else:
+            nums[abs(num)-1]*=(-1)
+    for i in range(len(nums)):
+        if nums[i]>0:
+            res.append(i+1)
+            return res
