b07606ee489fb60f3ad09c8eabb1b14b96a9ec09
prajwalap <praj.p98@gmail.com>
2025-05-08 12:19:36 +0530

    744. Find Smallest Letter Greater Than Target-2

--- 0744 Find Smallest Letter Greater Than Target.py
+++ 0744 Find Smallest Letter Greater Than Target.py
@@ -3,3 +3,17 @@ def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         if letter > target:
             return letter
     return letters[0]
+
+def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+    l=0
+    r=len(letters)-1
+    while l<=r:
+        mid=(l+r)//2
+        if letters[mid]<=target:
+            l=mid+1
+        else:
+            r=mid-1
+    if l==len(letters):
+        return letters[0]
+    else:
+        return letters[l]
