adec4695a5ffb92601b3127020610a561b1ee97a
Prajwala <praj.p98@gmail.com>
2025-05-12 14:06:24 +0530

    507. Perfect Number

---
+++ 0507 Perfect Number.py
@@ -0,0 +1,8 @@
+def checkPerfectNumber(self, num: int) -> bool:
+    if num == 1:
+        return False
+    total = 1
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            total += i + num // i
+    return total == num
