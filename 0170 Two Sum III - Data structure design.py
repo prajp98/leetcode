7b524fd95b1b57a5412f6c6aaa0daf12e632f8ed
Prajwala <praj.p98@gmail.com>
2025-05-12 18:03:22 +0530

    170. Two Sum III - Data structure design

---
+++ 0170 Two Sum III - Data structure design.py
@@ -0,0 +1,20 @@
+class TwoSum:
+
+    def __init__(self):
+        self.nums = {}
+
+    def add(self, number: int) -> None:
+        if number in self.nums:
+            self.nums[number] += 1
+        else:
+            self.nums[number] = 1
+
+    def find(self, value: int) -> bool:
+        for num in self.nums.keys():
+            comple = value - num
+            if num != comple:
+                if comple in self.nums:
+                    return True
+            elif self.nums[num] > 1:
+                return True
+        return False
