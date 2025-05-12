e9cd2288625ae0973f69515a6749cfe6fc620364
Prajwala <praj.p98@gmail.com>
2025-05-12 15:44:03 +0530

    400. Nth Digit

---
+++ 0400 Nth Digit.py
@@ -0,0 +1,13 @@
+def findNthDigit(self, n: int) -> int:
+    length = 1
+    count = 9
+    start = 1
+
+    while n > length * count:
+        n -= length * count
+        length += 1
+        count *= 10
+        start *= 10
+    num = start + (n - 1) // length
+    i = (n - 1) % length
+    return int(str(num)[i])
