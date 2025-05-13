86253205d7c56e34f5ba7765fe89f2eb6a39dd51
prajwalaparameshwar <praj.p98@gmail.com>
2025-05-13 13:02:37 +0530

    340. Longest Substring with At Most K Distinct Characters

--- 0484 Find Permutation.py
+++ 0484 Find Permutation.py
@@ -7,3 +7,19 @@ def findPermutation(self, s: str) -> list[int]:
             while stack:
                 res.append(stack.pop())
     return res
+
+def findPermutation(self, s: str) -> list[int]:
+    n = len(s)
+    res = list(range(1, n + 2))  # Initial permutation [1 to n+1]
+
+    i = 0
+    while i < n:
+        if s[i] == 'D':
+            start = i
+            while i < n and s[i] == 'D':
+                i += 1
+            res[start:i+1] = reversed(res[start:i+1])
+        else:
+            i += 1
+
+    return res
