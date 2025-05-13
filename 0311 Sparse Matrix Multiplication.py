c931e75d6a1cab296086ce8088d92d648c86c3d0
prajwalaparameshwar <praj.p98@gmail.com>
2025-05-13 14:25:57 +0530

    311. Sparse Matrix Multiplication

--- 0311 Sparse Matrix Multiplication.py
+++ 0311 Sparse Matrix Multiplication.py
@@ -6,3 +6,15 @@ def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[in
             for x in range(k):
                 res[r][c] += mat1[r][x] * mat2[x][c]
     return res
+
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        m, n, k = len(mat1), len(mat1[0]), len(mat2[0])
+        res = [[0] * k for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if mat1[i][j] != 0:
+                    for x in range(k):
+                        if mat2[j][x] != 0:
+                            res[i][x] += mat1[i][j]*mat2[j][x]
+        return res
