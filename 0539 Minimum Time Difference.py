f3717dbe4e06139f5d3638e1e91d7c6d65e26e8b
prajwalap <praj.p98@gmail.com>
2025-05-15 12:19:33 +0530

    539. Minimum Time Difference-2

--- 0539 Minimum Time Difference.py
+++ 0539 Minimum Time Difference.py
@@ -10,3 +10,24 @@ def findMinDifference(self, timePoints: List[str]) -> int:
         res = min(res, times[i + 1] - times[i])
     res = min(res, 1440 + times[0] - times[-1])
     return res
+
+def findMinDifference(self, timePoints: List[str]) -> int:
+    if len(timePoints) > 1440:
+        return 0
+    seen = [False] * 1440
+    for time in timePoints:
+        t = int(time[:2]) * 60 + int(time[3:])
+        if seen[t]:
+            return 0
+        seen[t] = True
+    prev=first=last=-1
+    res=1440
+    for i in range(1440):
+        if seen[i]:
+            if prev!=-1:
+                res=min(res,i-prev)
+            else:
+                first = i
+            prev = i
+    res=min(res,1440-prev+first)
+    return res
