459ee526205659777280f0cd551b876bb5279206
prajwalap <praj.p98@gmail.com>
2025-05-16 12:26:21 +0530

    287. Find the Duplicate Number-2

--- 0287 Find the Duplicate Number.py
+++ 0287 Find the Duplicate Number.py
@@ -4,3 +4,17 @@ def findDuplicate(self, nums: List[int]) -> int:
         if nums[index] < 0:
             return index
         nums[index] *= -1
+
+def findDuplicate(self, nums: List[int]) -> int:
+    slow = fast = 0
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    slow = 0
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    return slow
