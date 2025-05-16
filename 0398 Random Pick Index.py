0bd79f64287b5e9cd4756555175226817e10af09
prajwalap <praj.p98@gmail.com>
2025-05-16 11:41:54 +0530

    398. Random Pick Index

---
+++ 0398 Random Pick Index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def __init__(self, nums):
+        self.indices = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.indices[num].append(i)
+
+    def pick(self, target):
+        return random.choice(self.indices[target])
