e816a8fae17cc72bc243865df88f96eeafd92c34
prajwalap <praj.p98@gmail.com>
2025-05-22 13:48:09 +0530

    56. Merge Intervals

---
+++ 0056 Merge Intervals.py
@@ -0,0 +1,12 @@
+def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    if not intervals:
+        return []
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
+    for curr in intervals[1:]:
+        last = merged[-1]
+        if curr[0] <= last[1]:
+            last[1] = max(last[1], curr[1])
+        else:
+            merged.append(curr)
+    return merged
