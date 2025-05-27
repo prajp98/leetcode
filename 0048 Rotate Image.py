f67a9694f42516e6db47c72edf59ec3997376090
Prajwala <praj.p98@gmail.com>
2025-05-27 14:42:08 +0530

    48. Rotate Image-2

--- 0048 Rotate Image.py
+++ 0048 Rotate Image.py
@@ -12,3 +12,16 @@ def rotate(self, matrix: List[List[int]]) -> None:
             matrix[u + i][r] = t
         r -= 1
         l += 1
+
+def rotate(self, matrix: List[List[int]]) -> None:
+    # reverse
+    l = 0
+    r = len(matrix) -1
+    while l < r:
+        matrix[l], matrix[r] = matrix[r], matrix[l]
+        l += 1
+        r -= 1
+    # transpose
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
