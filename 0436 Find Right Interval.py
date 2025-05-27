ffb340a45a15c8ed24a63e9a9a1506b63df9822a
Prajwala <praj.p98@gmail.com>
2025-05-27 12:17:48 +0530

    436. Find Right Interval-1

---
+++ 0436 Find Right Interval.py
@@ -0,0 +1,18 @@
+def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+    n = len(intervals)
+    starti = sorted((start, i) for i, (start, _) in enumerate(intervals))
+    res = []
+    for interval in intervals:
+        end = interval[1]
+        # Binary search for the smallest start >= end
+        l, r = 0, n - 1
+        i = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if starti[mid][0] >= end:
+                i = starti[mid][1]
+                r = mid - 1  # Try to find smaller valid start
+            else:
+                l = mid + 1
+        res.append(i)
+    return res
