16d1f4ed5fa68058a7f0796e6e6352e33f0dbfdb
Prajwala <praj.p98@gmail.com>
2025-05-28 12:22:57 +0530

    162. Find Peak Element

--- 0162 Find Peak Element.py
+++ 0162 Find Peak Element.py
@@ -1,5 +1,5 @@
 def findPeakElement(self, nums: List[int]) -> int:
-    r = len(nums)
+    l, r = 0, len(nums) - 1
     while l < r:
         mid = (l + r) // 2
         if nums[mid] > nums[mid + 1]:
