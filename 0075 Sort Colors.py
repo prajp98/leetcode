8e5fad4e2cba4a86336b58a9de1c8fc96383d945
Prajwala <praj.p98@gmail.com>
2025-05-28 12:01:38 +0530

    75. Sort Colors

---
+++ 0075 Sort Colors.py
@@ -0,0 +1,14 @@
+def sortColors(self, nums: List[int]) -> None:
+    l = 0
+    r = len(nums) - 1
+    i = 0
+    while i <= r:
+        if nums[i] == 0:
+            nums[i], nums[l] = nums[l], nums[i]
+            i += 1
+            l += 1
+        elif nums[i] == 1:
+            i += 1
+        else:
+            nums[i], nums[r] = nums[r], nums[i]
+            r -= 1
