68cbbee90641d84babb1cdd7e84ce5980c7e85f6
Prajwala <praj.p98@gmail.com>
2025-06-02 13:25:31 +0530

    586. Customer Placing the Largest Number of Orders

--- SQL/0586 Customer Placing the Largest Number of Orders.sql
+++ SQL/0586 Customer Placing the Largest Number of Orders.sql
@@ -3,3 +3,14 @@ FROM Orders
 GROUP BY customer_number
 ORDER BY COUNT(*) DESC
 LIMIT 1;
+
+SELECT customer_number
+FROM orders
+GROUP BY customer_number
+HAVING COUNT(order_number) =
+(
+SELECT MAX(count) from
+    (
+     SELECT COUNT(order_number) as count, customer_number from orders group by customer_number
+     ) sub1
+);
