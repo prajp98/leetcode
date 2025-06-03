c9f321dcb3eacea2f6b4986d2153fc438a2309b7
prajwalap <praj.p98@gmail.com>
2025-06-03 16:40:39 +0530

    176. Second Highest Salary

--- SQL/0176.Second Highest Salary.sql
+++ SQL/0176.Second Highest Salary.sql
@@ -4,3 +4,10 @@ SELECT
         ORDER BY Salary DESC
         LIMIT 1 OFFSET 1)
     AS SecondHighestSalary;
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM Employee
+);
