fcf189e31514d1d82fc9392e7b640d72e494fd90
Prajwala <praj.p98@gmail.com>
2025-07-16 11:23:45 +0530

    1929

---
+++ 1929 Concatenation of Array.py
@@ -0,0 +1,7 @@
+def getConcatenation(self, nums: List[int]) -> List[int]:
+    n = len(nums)
+    res = [0] * 2 * n
+    for i in range(n):
+        res[i] = nums[i]
+        res[n + i] = nums[i]
+    return res
